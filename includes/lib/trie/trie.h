14d9b0e97a09fe993ebb806ef1c598d3aa3f3e61
serenity0204 <yuhenglin0204@gmail.com>
2023-01-11 20:43:28 -0800

    added basic structure

---
+++ includes/lib/trie/trie.h
@@ -0,0 +1,17 @@
+#ifndef TRIE_H
+#define TRIE_H
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+template<class T>
+class Trie
+{
+private:
+
+public:
+};
+
+    
+#endif // !TRIE_H
---
+++ includes/lib/trie_node/trie_node.h
@@ -0,0 +1,15 @@
+#ifndef TRIE_NODE_H
+#define TRIE_NODE_H
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+template<typename T>
+struct TrieNode
+{
+private:
+public:
+};
+    
+#endif // !TRIE_NODE_H
